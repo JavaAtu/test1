fee1a056bf7b5da9e5c85fbf967a267d0b43f789
Artur B <abartosiak@interia.pl>
2017-03-21 23:29:13 +0100

    ver2

--- src/Test1.java
+++ src/Test1.java
@@ -3,6 +3,7 @@
  */
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("V1");
+        System.out.println("V2");
+        System.out.println("XXX");
     }
 }
