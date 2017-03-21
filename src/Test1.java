c1eea82979d80e025847c51852a0755ef9963c71
Artur B <abartosiak@interia.pl>
2017-03-21 23:47:35 +0100

    ver1 - part 2

--- src/Test1.java
+++ src/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("V2");
         System.out.println("XXX");
+        System.out.println("YYY");
     }
 }
